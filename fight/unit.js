21bb0856ebf117d64d420dc875486764baf00ee1
Denis <den_85@ukr.net>
2019-06-25 23:14:39 +0300

    Update unit.js

--- fight/unit.js
+++ fight/unit.js
@@ -1,69 +1,70 @@
 function Unit(name) {
-    this.maxHealth = 100;
-    this.health = this.maxHealth;
-    this.name =  name;
-    this.level = 1;
-    this.xp = 0;
+    this.MAXHEALTH = 100;// more fun with 1000
+    this._health = this.MAXHEALTH;
+    this._name =  name;
+    this._level = 1;
+    this._xp = 0;
 }
 Unit.prototype.getName = function(){
-    return this.name;
+    return this._name;
 }
 
 Unit.prototype.getLevel = function () {
-    return this.level;
+    return this._level;
 }
 Unit.prototype.isAlive = function () {
-    return !!this.health;
+    return !!this._health;
 }
 Unit.prototype.addHealth = function (amount) {
-    if (this.health + Number(amount) > this.maxHealth)
-        this.health = this.maxHealth ;
+    if (this._health + Number(amount) > this.MAXHEALTH)
+        this._health = this.MAXHEALTH ;
     else
-        this.health += Number(amount);
+        this._health += Number(amount);
 }
 
-Unit.prototype.earnExperience = function (amount) {
+Unit.prototype._earnExperience = function (amount) {
 
 
-    let bonusXP = (this.level > 1) ? (amount - amount / this.level * 0.1) : amount;
+    let bonusXP = (this.getLevel() > 1) ? (amount - amount / this.getLevel() * 0.1) : amount;
 
 
-    if (this.xp + bonusXP > 1000){
+    if (this.getXP() + bonusXP > 1000){
 
         while (bonusXP > 1000) {
-            this.levelUp();
+            this._levelUp();
             bonusXP -= 1000;
         }
-        this.levelUp();
-        this.xp = Number(bonusXP);
+        this._levelUp();
+        this._xp = Number(bonusXP);
     }
     else{
-        this.xp += Number(bonusXP);
+        this._xp += Number(bonusXP);
     }
 
 }
 
 Unit.prototype.takeDamage = function (amount) {
-    if (this.health - amount > 0)
-        this.health -= amount;
+
+    if (this._health - amount > 0)
+        this._health -= amount;
     else
-        this.health = 0;
-    this.earnExperience(500);
+        this._health = 0;
+    this._earnExperience(500);
 }
 
-Unit.prototype.levelUp = function () {
-    this.level += 1;
-    console.log("- fighter " + this.name + " has reached level " + this.level);
+Unit.prototype._levelUp = function () {
+    this._level += 1;
+    console.log("- fighter " + this._name + " has reached level " + this._level);
 }
 
 
 
 //---------------------------------------------
 Unit.prototype.getHealth = function(){
-    return this.health;
+    return this._health;
 }
 Unit.prototype.getXP = function(){
-    return this.xp;
+    return this._xp;
 }
 
 module.exports =  Unit;
