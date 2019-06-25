c708aa8e665dd41b7e358707b93c006f069fa381
Denis <den_85@ukr.net>
2019-06-25 23:13:04 +0300

    Update doctor.js

--- fight/doctor.js
+++ fight/doctor.js
@@ -2,23 +2,26 @@ const Unit = require('./unit');
 
 function Doctor(){
     Unit.apply(this, arguments);
-    this.healPower = 10;
+    this._healPower = 10;
 }
 
 Doctor.prototype = new Unit();
 
 Doctor.prototype.heal = function (unit) {
 
-    let bonusAttack = (this.level > 1) ? (this.level * 0.1) : 0;
+    let bonusAttack = (this._level > 1) ? (this._level * 0.1) : 0;
 
-    let healthBefore = unit.getHealth();
-    unit.addHealth(this.healPower + bonusAttack);
-    let healthAfter = unit.getHealth();
+   // let healthBefore = unit.getHealth();
+    unit.addHealth(this._healPower + bonusAttack);
+   // let healthAfter = unit.getHealth();
 
-    unit.earnExperience(250);
-    this.earnExperience(250);//xp to doc as well
+    unit._earnExperience(250);
+    this._earnExperience(250);
 
-    return healthAfter - healthBefore;
+    //exact damage taken
+    //return healthAfter - healthBefore;
+
+    return (this._healPower + bonusAttack);
 }
 
 module.exports =  Doctor;
