23d77946078f01f5a4b949f4ce5c1c976146e4a2
Denis <den_85@ukr.net>
2019-06-25 23:13:42 +0300

    Update heavy.js

--- fight/heavy.js
+++ fight/heavy.js
@@ -3,35 +3,30 @@ const Unit = require('./unit');
 
 function Heavy() {
     Unit.apply(this, arguments);
-    this.resist = 20;
+    this._resist = 20;
 }
 Heavy.prototype = new Soldier();
 
 Heavy.prototype.machineGunAttack = function(unit, noOfShots){
+
     let bonusAttack, damage = 0, healthBefore, healthAfter;
 
     for(let index = 0; index < noOfShots; index++) {
-        bonusAttack = (this.level > 1) ? (this.level * 0.1) : 0;
+        bonusAttack = (this._level > 1) ? (this._level * 0.1) : 0;
         healthBefore = unit.getHealth();
-        unit.takeDamage(this.attackPower + bonusAttack);
+        unit.takeDamage(this._attackPower + bonusAttack);
         healthAfter = unit.getHealth();
         damage += healthBefore - healthAfter;
     }
 
-    this.earnExperience(250);
+    this._earnExperience(250);
 
     return damage;
 }
 
 Heavy.prototype.takeDamage = function (amount) {
-    amount = amount * (100 - this.resist)/100;
-
-    if (this.health - Number(amount) > 0)
-        this.health -= Number(amount) ;
-    else
-        this.health = 0;
-    let bonusXP = (this.level > 1) ? (500 - 500 / this.level * 0.1) : 500;
-    this.earnExperience(bonusXP);
+    amount = amount * (100 - this._resist)/100;
+    Unit.prototype.takeDamage.call(this, amount);
 }
 
 module.exports = Heavy;
